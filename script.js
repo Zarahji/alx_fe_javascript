fa58701f3711749f762072944952f4a04adcbefd
Zarahji <chukolfatima@gmail.com>
2025-02-02 12:50:57 +0100

    sections added

--- script.js
+++ script.js
@@ -5,18 +5,11 @@ const quotes = [
     { text: "The only way to do great work is to love what you do.", category: "Inspiration" },
     { text: "Life is what happens when you're busy making other plans.", category: "Life" }
   ];
-  
-  // Function to display a random quote
   function showRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const quote = quotes[randomIndex];
     document.getElementById('quoteDisplay').innerText = `${quote.text} - ${quote.category}`;
   }
-  
-  // Event listener for "Show New Quote" button
-  document.getElementById('newQuote').addEventListener('click', showRandomQuote);
-  
-  // Function to add a new quote
   function addQuote() {
     const quoteText = document.getElementById('newQuoteText').value;
     const quoteCategory = document.getElementById('newQuoteCategory').value;
@@ -29,4 +22,5 @@ const quotes = [
       alert('Please enter both a quote and a category.');
     }
   }
-  
+  document.getElementById('newQuote').addEventListener('click', showRandomQuote);
+      
